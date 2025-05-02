f9492f251c14ed14e89afa014d96857d8fd49127
='fauz <='fauzgabriel@gmail.com>
2025-05-02 11:27:21 +0700

    list detail screen

--- lib/list_detail_extension.dart
+++ lib/list_detail_extension.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:list_detail_extension/AppExtension.dart';
+import 'package:list_detail_extension/app_extension.dart';
 import 'package:list_detail_extension/pustaka_detail_screen.dart'
     deferred as detail_screen;
 
